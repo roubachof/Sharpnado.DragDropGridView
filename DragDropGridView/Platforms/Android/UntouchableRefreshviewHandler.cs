12e6aac662392e360404c1deb25ef57053d168b3
Pedro Jesus <pedrojesus@Pedros-MacBook-Pro.local>
2025-10-27 21:48:49 -0300

    fix typo

--- DragDropGridView/Platforms/Android/UntouchableRefreshviewHandler.cs
+++ DragDropGridView/Platforms/Android/UntouchableRefreshviewHandler.cs
@@ -9,7 +9,7 @@ public class UntouchableRefreshViewHandler : RefreshViewHandler
 {
     public void UpdateDisableScrolling(bool disableScrolling)
     {
-        ((UntouchableMauiSwipeRefreshLayout)PlatformView).UodateDisableScrolling(disableScrolling);
+        ((UntouchableMauiSwipeRefreshLayout)PlatformView).UpdateDisableScrolling(disableScrolling);
     }
 
     protected override UntouchableMauiSwipeRefreshLayout CreatePlatformView()
@@ -29,7 +29,7 @@ public class UntouchableMauiSwipeRefreshLayout : MauiSwipeRefreshLayout
     {
     }
 
-    public void UodateDisableScrolling(bool disableScrolling)
+    public void UpdateDisableScrolling(bool disableScrolling)
     {
         _disableScrolling = disableScrolling;
     }
